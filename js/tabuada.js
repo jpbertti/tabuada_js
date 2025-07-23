fc542b35c3067825d46b4440e750c60d752accd7
João Pedro Bertti Silva <joaobertty@gmail.com>
2025-07-23 19:36:40 -0300

    Initial commit

---
+++ js/tabuada.js
@@ -0,0 +1,20 @@
+let botoes = '';
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+    let botao = `<button name="botao" class = "btn btn-primary">${i}</button>`
+    
+    botoes = botoes + '&nbsp' + botao;
+    // botoes += `&nbsp;${botao}`;
+    // console.log(botoes);
+}
+
+document.getElementById("valores").innerHTML = botoes;
+
+
+const arrayBotao = document.getElementsByName("botao");
+
+console.log(typeof arrayBotao);
+console.log(arrayBotao);
+console.log(arrayBotao.length);
+console.log(arrayBotao[10].innerText);
